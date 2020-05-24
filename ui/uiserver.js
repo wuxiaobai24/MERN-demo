5209c67eceec105fb2bac8465e7e4af3bc9ed302
wuxiaobai24 <wuxiaobai24@foxmail.com>
2020-05-24 17:17:02 +0800

    Add dotenv

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
+require("dotenv").config();
+const port = process.env.UI_SERVER_PORT || 8000;
+const UI_API_ENDPOINT =
+  process.env.UI_API_ENDPOINT || "httpL//localhost:3000/graphql";
+const env = { UI_API_ENDPOINT };
+
 const app = express();
+
+app.get("/env.js", function (req, res) {
+  res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
 
 app.use(express.static("public"));
 
-app.listen(8000, function () {
-  console.log("UI started on port 8000");
+app.listen(port, function () {
+  console.log(`UI started on port ${port}`);
 });
