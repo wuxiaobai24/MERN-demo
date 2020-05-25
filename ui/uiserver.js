e8bdaca9fbea7c21908c1142c2dbebdb7c57286b
wuxiaobai24 <wuxiaobai24@foxmail.com>
2020-05-25 10:38:20 +0800

    Add ESLint

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.UI_SERVER_PORT || 8000;
 const UI_API_ENDPOINT =
-  process.env.UI_API_ENDPOINT || "httpL//localhost:3000/graphql";
+  process.env.UI_API_ENDPOINT || 'httpL//localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
 const apiProxyTarget = process.env.API_PROXY_TARGET;
-console.log(apiProxyTarget, env)
+console.log(apiProxyTarget, env);
 const app = express();
 if (apiProxyTarget) {
   app.use(
-    "/graphql",
+    '/graphql',
     createProxyMiddleware({ target: apiProxyTarget, changeOrigin: true })
   );
 }
 
-app.get("/env.js", function (req, res) {
+app.get('/env.js', function (req, res) {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.listen(port, function () {
   console.log(`UI started on port ${port}`);
